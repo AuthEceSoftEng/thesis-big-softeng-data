ac3bd0f8bd246e340b98a9af3c658d3e2497e281
styltopa <steltop12@gmail.com>
2025-03-26 15:59:35 +0200

    Bash script to delete and recreate topic

---
+++ events-to-cassandra-dockerized-system/usrlib/delete_and_recreate_topic.sh
@@ -0,0 +1,15 @@
+# # Single partition
+# docker exec -i kafka bash -c 'opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic historical-raw-events ; sleep 5; opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --topic historical-raw-events --partitions 1 --replication-factor 1'
+
+# Two or four partitions
+docker exec -i kafka bash -c 'opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic historical-raw-events ; sleep 5; opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --topic historical-raw-events --partitions 4 --replication-factor 1'
+
+
+
+#  opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --topic historical-raw-events --partitions 1 --replication-factor 1'
+
+# # Delete topic messages through deleting the topic
+#  opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic historical-raw-events
+
+# # Recreate the topic
+#  opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --topic historical-raw-events --partitions 1 --replication-factor 1
