a5f3b9b7d4839606267776877ae12e408a879d05
styltopa <steltop12@gmail.com>
2025-03-31 23:44:51 +0300

    Added env variables for jvm metaspace memory config in flink jobmanager and taskmanager-1 docker services. Also, added the heavy thin of data function file

---
+++ events-to-cassandra-dockerized-system/usrlib/heavy_thin_of_data.py
@@ -0,0 +1,138 @@
+import json, gzip, os, sys
+import warnings
+
+from produce_from_last_line_of_file import extract_compressed_file_from_path
+# import get_parsed_gharchive_files import save_files_parsed, restore_parsed_files
+
+
+from collections import defaultdict
+
+glob_counter = 0
+
+
+def heavy_process_json_event(event):
+	"""
+	Function that receives as input an event and keeps only fields of interest.
+	"""
+	
+	
+ 
+	new_event = defaultdict(dict)
+	try:
+		new_event = {
+      		"id": event["id"],
+			"type": event["type"],	
+   			"actor": event["actor"]["login"],
+			"repo": {
+       			"full_name": event["repo"]["name"]
+          	},
+			"created_at": event["created_at"]
+		}
+		if new_event["type"] == "PushEvent":
+			new_event["payload"] = {
+				"distinct_size": event["payload"]["distinct_size"],
+    			"size": event["payload"]["size"],
+			}		
+			# print(new_event)
+			# raise Exception("Check the event payload")
+		elif new_event["type"] == "PullRequestEvent":
+			new_event["payload"] = {
+				"action": event["payload"]["action"],
+				"pull_request": {
+					"number": event["payload"]["pull_request"]["number"],
+					"user": event["payload"]["pull_request"]["user"]["login"],				
+     				"commits": event["payload"]["pull_request"]["commits"],
+					"created_at": event["payload"]["pull_request"]["created_at"],
+     				"merged_at": event["payload"]["pull_request"]["merged_at"],
+					"closed_at": event["payload"]["pull_request"]["closed_at"]
+				}
+			}
+			# print(new_event)
+			# raise Exception("Check the event payload")
+		elif new_event["type"] == "IssuesEvent":
+			new_event["payload"] = {
+				"action": event["payload"]["action"],
+				"issue": {
+					"number": event["payload"]["issue"]["number"],
+					"created_at": event["payload"]["issue"]["created_at"],
+					"closed_at": event["payload"]["issue"]["closed_at"],
+					# "labels": event["payload"]["issue"]["labels"]
+     				"labels": [label_element["name"] for label_element in event["payload"]["issue"]["labels"]]
+				}		
+			}
+			# print(new_event)
+			# raise Exception("Check the event payload")	
+   
+	except KeyError as e:
+		raise KeyError(e)
+  
+	return new_event
+
+ 
+
+
+
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+
+
+def heavy_thin_data_of_file(input_filepath, output_filepath, do_again=False):
+	'''
+	Thins the events of the gharchive filepath `input_filepath` 
+	and stores it into `output_filepath`.
+	If the do_again is True, the decompression is done again and 
+	the existing file is overwritten
+ 
+	`input_filepath`: GHArchive file with events to thin data
+	`output_filepath`: GhArchive file with thinned events 
+	`do_again`: In case the GHArchuive file with the thinned events
+	already exists, thin the events again and overwrite the 
+	output_filepath contents
+	'''
+	# Calculate size of file (number of lines of file)
+	with gzip.open(input_filepath, 'r') as file_object:
+			linesInFile = len(file_object.readlines())
+
+	number_of_lines_thinned_per_print = 1000
+
+	print(f'Thinning events of {os.path.basename(input_filepath)}...')
+	if not os.path.exists(output_filepath) or do_again == True:
+		with gzip.open(output_filepath, 'wb') as outstream:
+			with gzip.open(input_filepath) as instream:
+				for i, line in enumerate(instream):
+					event = json.loads(line)
+					# print(f"Original event repo field: {event['repo']}")
+					# print(event["repo"]["full_name"])
+					thinned_event = heavy_process_json_event(event)	
+					thinned_line = json.dumps(thinned_event) + '\n'
+					outstream.write(thinned_line.encode())
+					# Print every e.g. 1000 lines thinned
+					if i % number_of_lines_thinned_per_print == 0:
+						sys.stdout.write("\r JSON events thinned: {0}/{1}".format(i+1, linesInFile))
+						sys.stdout.flush()
+						# if i == 10000:	
+						# 	# print(f"Thinned event repo field: {thinned_event['repo']}")
+						# 	break	
+				# Print again at the last line thinned			
+				sys.stdout.write("\r JSON events thinned: {0}/{1}".format(linesInFile, linesInFile))
+				sys.stdout.flush()
+							
+	elif os.path.exists(output_filepath) and do_again == False:
+		print(f"{os.path.basename(output_filepath)} with the thinned events already exists.")
+	print('...Done')
+  
+  
+  
+# # Demo
+# in_dir_path = '/home/xeon/thesis-big-softeng-data/events-to-cassandra-dockerized-system/usrlib/github_data_for_speed_testing'
+
+# input_filename = '2024-12-01-11.json.gz'
+# input_path = os.path.join(in_dir_path, input_filename)
+
+# out_dir_path = '/home/xeon/thesis-big-softeng-data/events-to-cassandra-dockerized-system/usrlib/github_data_for_speed_testing'
+# output_filename = '2024-12-01-11-heavy-thinned.json.gz'
+# output_path = os.path.join(out_dir_path, output_filename)
+
+
+# heavy_thin_data_of_file(input_path, output_path, do_again=True)
+# extract_compressed_file_from_path(output_path, do_again=True)
