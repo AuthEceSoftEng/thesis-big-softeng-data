6f0037d192164c3a85b09dd26cae7e1b4a4c3d36
styltopa <steltop12@gmail.com>
2025-06-16 13:20:54 +0300

    Most popular languages by day cassandra sink

--- events-to-cassandra-dockerized-system/usrlib/test_near_real_time_queries.py
+++ events-to-cassandra-dockerized-system/usrlib/test_near_real_time_queries.py
@@ -35,16 +35,16 @@ try:
         # session.execute(create_stats_table)
 
 
-        popular_languages_table = "popular_languages_by_day"
+        popular_languages_table = "most_popular_languages_by_day"
         # create_pop_langs_table = \
         #         f"CREATE TABLE IF NOT EXISTS {keyspace}.{popular_languages_table} \
-        #         (day text, language text, num_of_occurences counter, PRIMARY KEY ((day), language)) WITH CLUSTERING ORDER BY (language desc);"
+        #         (day text, language text, num_of_occurrences counter, PRIMARY KEY ((day), language)) WITH CLUSTERING ORDER BY (language desc);"
         # session.execute(create_pop_langs_table)
 
-        popular_topics_table = "popular_topics_by_day"
+        popular_topics_table = "most_popular_topics_by_day"
         # create_pop_topics_table = \
         #         f"CREATE TABLE IF NOT EXISTS {keyspace}.{popular_topics_table} \
-        #         (day text, topic text, num_of_occurences counter, PRIMARY KEY ((day), topic)) WITH CLUSTERING ORDER BY (topic desc);"
+        #         (day text, topic text, num_of_occurrences counter, PRIMARY KEY ((day), topic)) WITH CLUSTERING ORDER BY (topic desc);"
         # session.execute(create_pop_topics_table)
 
 
@@ -79,7 +79,7 @@ try:
 
 
         # update_pop_langs = session.prepare(f"UPDATE {keyspace}.{popular_languages_table} " \
-        #                 "SET num_of_occurences = num_of_occurences + 1 WHERE day = ? and language = ?")
+        #                 "SET num_of_occurrences = num_of_occurrences + 1 WHERE day = ? and language = ?")
         # languages = ["Python", "C", "Java"]
         # day = "2025-06-15"
         # session.execute(update_pop_langs, 
@@ -87,7 +87,7 @@ try:
         
         
         # update_pop_topics = session.prepare(f"UPDATE {keyspace}.{popular_topics_table} " \
-        #                 "SET num_of_occurences = num_of_occurences + 1 WHERE day = ? and topic = ?")
+        #                 "SET num_of_occurrences = num_of_occurrences + 1 WHERE day = ? and topic = ?")
         # topics = ["hacktoberfest", "machine learning", "leet-code"]
         # day = "2025-06-15"
         # session.execute(update_pop_topics, 
@@ -129,54 +129,55 @@ try:
         
 
 
-        # select_langs_prepared_query = session.prepare(\
-        #         f"SELECT day, language, num_of_occurences "\
-        #         f"FROM {keyspace}.{popular_languages_table} WHERE day = ?")
-        # langs_queried_res = session.execute(select_langs_prepared_query, [day])            
-        # langs_queried_rows = langs_queried_res.all()
-        # langs_queried_rows_sorted = sorted(langs_queried_rows, key=lambda x: x.num_of_occurences, reverse=True)
-        # print(f"Languages on {day}:\n"\
-        #         "Language\tNumber of occurences")
-        # for i in range(len(langs_queried_rows_sorted)):
-        #         print(f"{langs_queried_rows_sorted[i].language}\t{langs_queried_rows_sorted[i].num_of_occurences}")
-        # print()
+        select_langs_prepared_query = session.prepare(\
+                f"SELECT day, language, num_of_occurrences "\
+                f"FROM {keyspace}.{popular_languages_table} WHERE day = ?")
+        langs_queried_res = session.execute(select_langs_prepared_query, [day])            
+        langs_queried_rows = langs_queried_res.all()
+        langs_queried_rows_sorted = sorted(langs_queried_rows, key=lambda x: x.num_of_occurrences, reverse=True)
+        print(f"Languages on {day}:\n"\
+                "Language\tNumber of occurrences")
+        max_num_of_langs_to_show = 5
+        for i in range(min(len(langs_queried_rows_sorted), max_num_of_langs_to_show)):
+                print(f"{langs_queried_rows_sorted[i].language}\t{langs_queried_rows_sorted[i].num_of_occurrences}")
+        print()
         
         
         # select_topics_prepared_query = session.prepare(\
-        #         f"SELECT day, topic, num_of_occurences "\
+        #         f"SELECT day, topic, num_of_occurrences "\
         #         f"FROM {keyspace}.{popular_topics_table} WHERE day = ?")
         # topics_queried_res = session.execute(select_topics_prepared_query, [day])            
         # topics_queried_rows = topics_queried_res.all()
-        # topics_queried_rows_sorted = sorted(topics_queried_rows, key=lambda x: x.num_of_occurences, reverse=True)
+        # topics_queried_rows_sorted = sorted(topics_queried_rows, key=lambda x: x.num_of_occurrences, reverse=True)
         # print(f"Topics on {day}:\n"\
-        #         "Topic\tNumber of occurences")
+        #         "Topic\tNumber of occurrences")
         # for i in range(len(topics_queried_rows_sorted)):
-        #         print(f"{topics_queried_rows_sorted[i].topic}\t{topics_queried_rows_sorted[i].num_of_occurences}")
+        #         print(f"{topics_queried_rows_sorted[i].topic}\t{topics_queried_rows_sorted[i].num_of_occurrences}")
         # print()
         
         
-        select_repos_prepared_query = session.prepare(\
-                f"SELECT day, repo, stars, forks "\
-                f"FROM {keyspace}.{most_popular_repos_table} WHERE day = ?")
-        repos_queried_res = session.execute(select_repos_prepared_query, [day])            
-        repos_queried_rows = repos_queried_res.all()
-        repos_queried_rows_sorted_by_stars = sorted(repos_queried_rows, key=lambda x: x.stars, reverse=True)
-        repos_queried_rows_sorted_by_forks = sorted(repos_queried_rows, key=lambda x: x.forks, reverse=True)
+        # select_repos_prepared_query = session.prepare(\
+        #         f"SELECT day, repo, stars, forks "\
+        #         f"FROM {keyspace}.{most_popular_repos_table} WHERE day = ?")
+        # repos_queried_res = session.execute(select_repos_prepared_query, [day])            
+        # repos_queried_rows = repos_queried_res.all()
+        # repos_queried_rows_sorted_by_stars = sorted(repos_queried_rows, key=lambda x: x.stars, reverse=True)
+        # repos_queried_rows_sorted_by_forks = sorted(repos_queried_rows, key=lambda x: x.forks, reverse=True)
         
-        max_number_of_repos_to_show = 5
+        # max_number_of_repos_to_show = 5
         
-        print(f"Most popular repos by stars on {day}:\n"\
-                "Repo\tStars")
-        for i in range(min(len(repos_queried_rows_sorted_by_stars), max_number_of_repos_to_show)):
-                print(f"{repos_queried_rows_sorted_by_stars[i].repo}\t{repos_queried_rows_sorted_by_stars[i].stars}")
-        print()
+        # print(f"Most popular repos by stars on {day}:\n"\
+        #         "Repo\tStars")
+        # for i in range(min(len(repos_queried_rows_sorted_by_stars), max_number_of_repos_to_show)):
+        #         print(f"{repos_queried_rows_sorted_by_stars[i].repo}\t{repos_queried_rows_sorted_by_stars[i].stars}")
+        # print()
         
         
-        print(f"Most popular repos by forks on {day}:\n"\
-                "Repo\nForks")
-        for i in range(min(len(repos_queried_rows_sorted_by_forks), max_number_of_repos_to_show)):
-                print(f"{repos_queried_rows_sorted_by_forks[i].repo}\t{repos_queried_rows_sorted_by_forks[i].forks}")
-        print()
+        # print(f"Most popular repos by forks on {day}:\n"\
+        #         "Repo\nForks")
+        # for i in range(min(len(repos_queried_rows_sorted_by_forks), max_number_of_repos_to_show)):
+        #         print(f"{repos_queried_rows_sorted_by_forks[i].repo}\t{repos_queried_rows_sorted_by_forks[i].forks}")
+        # print()
         
         # print("Check if the table is sorted")
         
