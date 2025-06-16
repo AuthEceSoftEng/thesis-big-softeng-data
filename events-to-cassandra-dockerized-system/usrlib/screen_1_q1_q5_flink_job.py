9e7244d22e4611be6972ae32be86e073e2555e39
styltopa <steltop12@gmail.com>
2025-06-16 12:39:30 +0300

    Cassandra support for most popular repos by day

--- events-to-cassandra-dockerized-system/usrlib/screen_1_q1_q5_flink_job.py
+++ events-to-cassandra-dockerized-system/usrlib/screen_1_q1_q5_flink_job.py
@@ -60,34 +60,259 @@ kafka_bootstrap_servers = config_parser['default_consumer']['bootstrap.servers']
 
 
 
-
-
-# stats_by_day
-# region
-topic_to_consume_from = "near-real-time-raw-events"
+# Common configs for all datastream transformations
+near_real_time_events_topic = "near-real-time-raw-events"
 kafka_props = {'enable.auto.commit': 'true',
                'auto.commit.interval.ms': '1000',
                'auto.offset.reset': 'smallest'}
+def map_event_string_to_event_dict(event_string):
+    return eval(event_string)
+
+
+
+
+
+# # Q1. stats_by_day
+# # region
+# stats_consumer_group_id = 'raw_events_to_stats_by_day_consumer_group'
+# kafka_consumer_stats = KafkaSource.builder() \
+#             .set_bootstrap_servers(kafka_bootstrap_servers) \
+#             .set_starting_offsets(KafkaOffsetsInitializer.committed_offsets(KafkaOffsetResetStrategy.EARLIEST)) \
+#             .set_group_id(stats_consumer_group_id)\
+#             .set_topics(near_real_time_events_topic) \
+#             .set_value_only_deserializer(SimpleStringSchema()) \
+#             .set_properties(kafka_props)\
+#             .build()
+
+            
+
+# raw_events_to_stats_ds = env.from_source( source=kafka_consumer_stats, \
+#             watermark_strategy=WatermarkStrategy.no_watermarks(),
+#             source_name="kafka_source")\
+#             .map(map_event_string_to_event_dict)\
+#                 # .set_parallelism(16)
+# print(f"Started reading data from kafka topic '{near_real_time_events_topic}' to create "
+#         "topic 'stats_by_day'")
+
+
+
+
+# def filter_no_statistics_events(eventDict):
+    
+#     event_type = eventDict["type"]
+#     if event_type == "PushEvent":
+#         # Keep PushEvents if number_of_conmmits <= 200 or if size == distinct_size
+#         number_of_commits = eventDict["payload"]["distinct_size"]
+#         if (number_of_commits <= 100) or (number_of_commits <= 200
+#         and eventDict["payload"]["size"] == eventDict["payload"]["distinct_size"]):
+#             return True
+#         else:
+#             return False
+#     elif event_type == "WatchEvent":
+#         return True
+#     elif event_type == "ForkEvent":
+#         return True
+#     elif event_type == "PullRequestEvent":
+#         # Filter out PullRequestEvents that do not open or close issues
+#         if eventDict["payload"]["action"] == "opened" or \
+#             eventDict["payload"]["action"] == "reopened":
+#             return True
+#     # If none of the above works, return False (filter the event out)
+#     return False
+    
+
+# def extract_statistics_and_create_row(eventDict):
+
+#     # (Example) From 2024-01-01T00:00:01Z get 2024-01-01  
+#     day = eventDict["created_at"].split('T', 1)[0]
+#     # Initialize the statistics per day 
+#     # for the cassandra update counter columns query
+#     commits = 0
+#     stars = 0
+#     forks = 0
+#     pull_requests = 0
+    
+#     event_type = eventDict["type"]                        
+#     if event_type == "PushEvent":
+#         commits = eventDict["payload"]["distinct_size"]
+#     elif event_type == "WatchEvent":
+#         stars = 1
+#     elif event_type == "ForkEvent":
+#         forks = 1
+#     elif event_type == "PullRequestEvent" and \
+#         (eventDict["payload"]["action"] == "opened" or \
+#             eventDict["payload"]["action"] == "reopened"):
+#         pull_requests = 1
+#     else: 
+#         raise ValueError(f"Event type {event_type} is not filtered")
+         
+#     return Row(commits, stars, forks, pull_requests, day)
+
+# stats_type_info = Types.ROW_NAMED(['commits', 'stars', 'forks', \
+#             'pull_requests', 'day'], [Types.LONG(), Types.LONG(),  \
+#             Types.LONG(), Types.LONG(), Types.STRING()])
+   
+
+
+
+# stats_ds = raw_events_to_stats_ds.filter(filter_no_statistics_events)\
+#                         .map(extract_statistics_and_create_row, \
+#                            output_type=stats_type_info)
+# cassandra_keyspace = "near_real_time_data"
+# stats_table = 'stats_by_day'
+# upsert_element_into_stats_by_day_q1 = \
+#             f"UPDATE {cassandra_keyspace}.{stats_table} \
+#                     SET commits = commits + ?, stars = stars + ?, \
+#                     forks = forks + ?, pull_requests = pull_requests + ? WHERE \
+#                     day = ?;"
+# cassandra_host = 'cassandra_stelios'
+# cassandra_port = 9142
+# max_concurrent_requests = 1000
+
+# print(f"Started inserting data from kafka topics into Cassandra tables:\n"
+#         "T1: stats_by_day, T2_3: most_popular_repos_by_day,\n"
+#         "T4: most_popular_languages_by_day, T5: most_popular_languages_by_day")
+# cassandra_sink_q1 = CassandraSink.add_sink(stats_ds)\
+#     .set_query(upsert_element_into_stats_by_day_q1)\
+#     .set_host(host=cassandra_host, port=cassandra_port)\
+#     .set_max_concurrent_requests(max_concurrent_requests)\
+#     .enable_ignore_null_fields()\
+#     .build()
+
+# # # Print transformed datastream
+# # stats_ds.print()
+
+# # endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Q2_3. most_popular_repos_by_day
+# # region
+# most_popular_repos_consumer_group_id = 'raw_events_to_most_popular_repos_by_day_consumer_group'
+# kafka_consumer_most_popular_repos = KafkaSource.builder() \
+#             .set_bootstrap_servers(kafka_bootstrap_servers) \
+#             .set_starting_offsets(KafkaOffsetsInitializer.committed_offsets(KafkaOffsetResetStrategy.EARLIEST)) \
+#             .set_group_id(most_popular_repos_consumer_group_id)\
+#             .set_topics(near_real_time_events_topic) \
+#             .set_value_only_deserializer(SimpleStringSchema()) \
+#             .set_properties(kafka_props)\
+#             .build()
+
+# raw_events_to_most_pop_repos_ds = env.from_source( 
+#             source=kafka_consumer_most_popular_repos, \
+#             watermark_strategy=WatermarkStrategy.no_watermarks(),
+#             source_name="kafka_source")\
+#             .map(map_event_string_to_event_dict)\
+#                 # .set_parallelism(16)
+# print(f"Started reading data from kafka topic '{near_real_time_events_topic}' to create "
+#         "topic 'most_popular_repos_by_day'")
+
+
+
+
+# def filter_non_watch_and_fork_events(eventDict):
+#     event_type = eventDict["type"]
+#     if event_type == "WatchEvent":
+#         return True
+#     elif event_type == "ForkEvent":
+#         return True
+#     # If none of the above works, return False (filter the event out)
+#     return False
+    
+
+# def extract_repo_stars_forks_and_create_row(eventDict):
+
+#     # (Example) From 2024-01-01T00:00:01Z get 2024-01-01  
+#     day = eventDict["created_at"].split('T', 1)[0]
+#     repo = eventDict["repo"]["full_name"]
+#     stars = 0
+#     forks = 0
+#     event_type = eventDict["type"]                        
+#     if event_type == "WatchEvent":
+#         stars = 1
+#     elif event_type == "ForkEvent":
+#         forks = 1
+#     else: 
+#         raise ValueError(f"Event with type '{event_type}' was not filtered")
+#     return Row(stars, forks, day, repo)
+
+
+# most_popular_repos_type_info = Types.ROW_NAMED(['stars', 'forks', \
+#             'day', 'repo'], [Types.LONG(), Types.LONG(),  \
+#             Types.STRING(), Types.STRING()])
+   
+
+
+
+# most_popular_repos_ds = raw_events_to_most_pop_repos_ds\
+#         .filter(filter_non_watch_and_fork_events)\
+#         .map(extract_repo_stars_forks_and_create_row, \
+#             output_type=most_popular_repos_type_info)
+        
+# cassandra_keyspace = "near_real_time_data"
+# most_popular_repos_table = 'most_popular_repos_by_day'
+# upsert_element_into_most_popular_repos_by_day_q2_3 = \
+#             f"UPDATE {cassandra_keyspace}.{most_popular_repos_table} \
+#                     SET stars = stars + ?, forks = forks + ? \
+#                     WHERE day = ? AND repo = ?;"
+# cassandra_host = 'cassandra_stelios'
+# cassandra_port = 9142
+# max_concurrent_requests = 1000
+
+# print(f"Started inserting data from {near_real_time_events_topic} into Cassandra table:\n"
+#         f"T2_3: '{most_popular_repos_table}'")
+# cassandra_sink_q2_3 = CassandraSink.add_sink(most_popular_repos_ds)\
+#     .set_query(upsert_element_into_most_popular_repos_by_day_q2_3)\
+#     .set_host(host=cassandra_host, port=cassandra_port)\
+#     .set_max_concurrent_requests(max_concurrent_requests)\
+#     .enable_ignore_null_fields()\
+#     .build()
+
+# # # Print transformed datastream
+# # most_popular_repos_ds.print()
+
+# # endregion
+
+
+
+
+# most_popular_languages_by_day
+
+# region
 stats_consumer_group_id = 'raw_events_to_stats_by_day_consumer_group'
 kafka_consumer_stats = KafkaSource.builder() \
             .set_bootstrap_servers(kafka_bootstrap_servers) \
             .set_starting_offsets(KafkaOffsetsInitializer.committed_offsets(KafkaOffsetResetStrategy.EARLIEST)) \
             .set_group_id(stats_consumer_group_id)\
-            .set_topics(topic_to_consume_from) \
+            .set_topics(near_real_time_events_topic) \
             .set_value_only_deserializer(SimpleStringSchema()) \
             .set_properties(kafka_props)\
             .build()
 
             
-def map_event_string_to_event_dict(event_string):
-    return eval(event_string)
 
-raw_events_ds = env.from_source( source=kafka_consumer_stats, \
+raw_events_to_stats_ds = env.from_source( source=kafka_consumer_stats, \
             watermark_strategy=WatermarkStrategy.no_watermarks(),
             source_name="kafka_source")\
             .map(map_event_string_to_event_dict)\
                 # .set_parallelism(16)
-print("Started reading data from kafka topic 'near-real-time-raw-events' to create "
+print(f"Started reading data from kafka topic '{near_real_time_events_topic}' to create "
         "topic 'stats_by_day'")
 
 
@@ -151,7 +376,7 @@ stats_type_info = Types.ROW_NAMED(['commits', 'stars', 'forks', \
 
 
 
-stats_ds = raw_events_ds.filter(filter_no_statistics_events)\
+stats_ds = raw_events_to_stats_ds.filter(filter_no_statistics_events)\
                         .map(extract_statistics_and_create_row, \
                            output_type=stats_type_info)
 cassandra_keyspace = "near_real_time_data"
@@ -175,17 +400,14 @@ cassandra_sink_q1 = CassandraSink.add_sink(stats_ds)\
     .enable_ignore_null_fields()\
     .build()
 
-
-
-
-
-
-
 # # Print transformed datastream
 # stats_ds.print()
 
 # endregion
 
+
+
+# most_popular_topics_by_day
 
 
 if __name__ =='__main__':
@@ -204,6 +426,7 @@ if __name__ =='__main__':
 
     
     # Screen 1
+    # stats by day
     stats_table = 'stats_by_day'
     create_stats_table =  \
                 f"CREATE TABLE IF NOT EXISTS {cassandra_keyspace}.{stats_table} \
@@ -212,6 +435,37 @@ if __name__ =='__main__':
     session.execute(create_stats_table)
 
 
+    # most_popular_repos_by_day
+    most_popular_repos_table = "most_popular_repos_by_day"
+    create_most_popular_repos_table = \
+            f"CREATE TABLE IF NOT EXISTS {cassandra_keyspace}.{most_popular_repos_table} \
+            (day text, repo text, stars counter, forks counter, PRIMARY KEY ((day), repo)) WITH CLUSTERING ORDER BY (repo desc);"
+    session.execute(create_most_popular_repos_table)
+    
+    
+    # most_popular_languages_by_day
+    popular_languages_table = "most_popular_languages_by_day"
+    create_pop_langs_table = \
+            f"CREATE TABLE IF NOT EXISTS {cassandra_keyspace}.{popular_languages_table} \
+            (day text, language text, num_of_occurences counter, PRIMARY KEY ((day), language)) WITH CLUSTERING ORDER BY (language desc);"
+    session.execute(create_pop_langs_table)
+
+
+    # # most_popular_topics_by_day
+    # popular_topics_table = "popular_topics_by_day"
+    # create_pop_topics_table = \
+    #         f"CREATE TABLE IF NOT EXISTS {cassandra_keyspace}.{popular_topics_table} \
+    #         (day text, topic text, num_of_occurences counter, PRIMARY KEY ((day), topic)) WITH CLUSTERING ORDER BY (topic desc);"
+    # session.execute(create_pop_topics_table)
+
+        
+    # No table to make for the near real time stars and forks section 
+    # (all is done with the datastream transformation and reenter into kafka topic 
+    # near-real-time-stars-forks)
+    
+    
+    
+    
 
 
     cluster.shutdown()
